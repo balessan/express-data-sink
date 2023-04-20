19c46fcc09499585fcf13e74a5f7417e368599cc
Benoit Alessandroni <benoit@happy-dev.fr>
2023-04-20 16:20:15 +0200

    fix: proper filename handling

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const fs = require("fs");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const ip = process.env.IP || '127.0.0.1';
+const ip = process.env.IP || "127.0.0.1";
 
 // Middleware to parse request body as JSON or binary
 app.use((req, res, next) => {
@@ -39,31 +39,33 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 // POST endpoint to write data to file
-app.post("/", (req, res) => {
+app.post("/sink/:filename", (req, res) => {
   const content = req.body;
-  const fileName = req.query.filename;
+  const fileName = req.params.filename;
+  const filePath = `./${fileName}`;
   if (!fileName) {
     res.status(400).send("Filename is required");
   } else {
     const jsonString = JSON.stringify(content);
-    fs.writeFile(fileName, jsonString, (err) => {
+    fs.writeFile(filePath, jsonString, (err) => {
       if (err) {
         console.error(err);
         res.status(500).send("Error writing to file");
       } else {
-        res.send({'message':'success'}).json();
+        res.send({ message: "success" }).json();
       }
     });
   }
 });
 
 // GET endpoint to read data from file
-app.get("/", (req, res) => {
-  const fileName = req.query.filename;
+app.get("/sink/:filename", (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = `./${fileName}`;
   if (!fileName) {
     res.status(400).send("Filename is required");
   } else {
-    fs.readFile(fileName, (err, data) => {
+    fs.readFile(filePath, (err, data) => {
       if (err) {
         console.error(err);
         res.status(500).send("Error reading file");
@@ -77,9 +79,9 @@ app.get("/", (req, res) => {
 
 // Default catch-all route to present API information
 app.use((req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-  });
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, ip, () => {
-  console.log("API running on port 3000");
+  console.log(`API running at http://${ip}:${port}`);
 });
